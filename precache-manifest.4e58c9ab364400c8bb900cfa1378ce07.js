23d3f74882ce558fd1aaa2d799398b5868e30207
junbinLiang <junbinliang816@gmail.com>
2019-05-23 06:30:20 -0400

    Updates

---
+++ precache-manifest.4e58c9ab364400c8bb900cfa1378ce07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da5ead3f4196515f8df30f7eb7a7edc2",
+    "url": "/2-player-Gomoku-Game/index.html"
+  },
+  {
+    "revision": "8a7aab15eb6d5b0c0ddb",
+    "url": "/2-player-Gomoku-Game/static/css/main.b72d26ac.chunk.css"
+  },
+  {
+    "revision": "58ad173cd721ecc045af",
+    "url": "/2-player-Gomoku-Game/static/js/2.44361bd4.chunk.js"
+  },
+  {
+    "revision": "8a7aab15eb6d5b0c0ddb",
+    "url": "/2-player-Gomoku-Game/static/js/main.09d63a70.chunk.js"
+  },
+  {
+    "revision": "979d4d4f58601ccad91d",
+    "url": "/2-player-Gomoku-Game/static/js/runtime~main.1516b14c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/2-player-Gomoku-Game/static/media/logo.5d5d9eef.svg"
+  }
+]);
